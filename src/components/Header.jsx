59369e5751b994f8148ef3334e6bbe6c94fdf4be
Dushyant418 <dushyantlochab335@gmail.com>
2024-05-13 09:13:53 +0530

    manage spacing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,14 +15,14 @@ const Header = () => {
             </div>
             <div className=' container max_width_1140'>
                 <div className='row  padding_top_110 pt-5 pt-lg-0 padding_bottom_96'>
-                    <div className='col-12 col-lg-7 d-flex align-items-end  justify-content-center justify-content-lg-start'>
+                    <div className='col-12 col-lg-8 d-flex align-items-center  justify-content-center justify-content-lg-start'>
                         <h2
-                            className='text-white m-0 fw-bold font_size_xxl font_family_Eurostile_Extended line_height_2xl max_width_640 pb-4 pb-lg-0'>
+                            className='text-white m-0 fw-bold font_size_xxl font_family_Eurostile_Extended line_height_2xl max_width_666 pb-4 pb-lg-0'>
                             Get The eBook That Reveals The <span className='text_yellow font_size_2xl line_height_3xl'>7 Fitness Myths</span> That Are Holding
                             You Back From Experiencing
                             Top-Level Fitness</h2>
                     </div>
-                    <div className='col-12 col-lg-5 d-flex  justify-content-center'>
+                    <div className='col-12 col-lg-4 d-flex  justify-content-center justify-content-lg-start'>
                         <img src={Header_image} alt='img1' className=' w-100 max_width_338'/>
                     </div>
                 </div>
--- src/components/Yellow_section.jsx
+++ src/components/Yellow_section.jsx
@@ -6,12 +6,12 @@ const Yellow_section = () => {
         <section className='Yellow_section'>
             <div className='container max_width_1140'>
                 <div className='row flex-column-reverse flex-lg-row pt-5 pb-5'>
-                    <div className='col-12 col-lg-7 d-flex align-items-center justify-content-center justify-content-lg-start'>
+                    <div className='col-12 col-lg-8 d-flex align-items-center justify-content-center justify-content-lg-start'>
                         <div className=' max_width_704'>
-                            <h2 className='fw-bold font_size_xl line_height_xxl mb-0 font_family_Eurostile_Extended text-capitalize'>With the right knowledge maintaining and improving your fitness becomes effortless and enjoyable. Stop wasting time you don’t have!</h2>
+                            <h2 className='fw-bold text-black font_size_xl line_height_xxl mb-0 font_family_Eurostile_Extended text-capitalize'>With the right knowledge maintaining and improving your fitness becomes effortless and enjoyable. Stop wasting time you don’t have!</h2>
                         </div>
                     </div>
-                    <div className='col-12 col-lg-5 d-flex justify-content-center pb-4 pb-lg-0'>
+                    <div className='col-12 col-lg-4 d-flex justify-content-center pb-4 pb-lg-0'>
                         <img className=' max_width_310 w-100' src={Card_image} alt="Card_image" />
                     </div>
                 </div>
